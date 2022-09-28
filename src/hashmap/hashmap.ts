153b373c8ed5d01517ed80585118d87f0d0dc399
John Ko <johnko426@gmail.com>
2022-09-28 14:00:29 -0700

    add to hashmap

---
+++ src/hashmap/hashmap.ts
@@ -0,0 +1,73 @@
+/**
+ * hashmap
+ * usage:
+ * const hashmap = new HashMap()
+ * hashmap.add('a', 1)
+ */
+
+
+export class HashMap<K extends unknown, V extends unknown> {
+  private size = 10
+  private items: [K, V][] = []
+  private ratio = 0.7
+  private current_size = 0
+  constructor() {
+    this.items = new Array(10)
+  }
+
+  set(key: K, value: V): V {
+    const hashIndex = this.hash(key)
+    const item = this.items[hashIndex]
+    this.items[hashIndex] = [key, value]
+
+    if (!item) {
+      this.current_size++
+
+      // see if it needs to double
+      this.need_resize()
+    }
+
+    return value
+  }
+
+  get(key: K): V | undefined {
+    const hashIndex = this.hash(key)
+    const item = this.items[hashIndex]
+    
+    if (item) {
+      return item[1]
+    }
+
+    return undefined
+  }
+
+  hash (key: K) {
+    if (typeof key === 'object') {}
+
+    return 5 % this.size
+  }
+
+  private calucate_ratio(): number {
+    return this.current_size / this.size
+  }
+
+  private need_resize() {
+    if (this.calucate_ratio() > this.ratio) {
+      this.rehash()
+    }
+  }
+
+  private rehash() {
+    // double size
+    this.size *= 2
+    const new_items = Array(this.size)
+    for (const item of this.items) {
+      const key = item[0]
+      const new_hash_index = this.hash(key)
+      new_items[new_hash_index] = item
+    }
+
+    this.items = new_items
+  }
+  
+}
